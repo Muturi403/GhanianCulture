e1ad4083ede50bec4ec3415ba73817c13113b776
Muturi403 <faith.muturi@student.moringaschool.com>
2021-06-13 17:15:17 +0300

    this will add the user interface logic

--- Script.js
+++ Script.js
@@ -8,5 +8,16 @@ var femaleNames = ["Akosua", "Adoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 function findNames(e){
 	e.preventDefault();
 	// alert("e");
-	
+	var day = parseInt(document.getElementsById("day").value);
+	var month = parseInt(document.getElementsById("month").value);
+	var year = parseInt(document.getElementsById("year").value);
+	var male = document.getElementById("male");
+	var female = document.getElementById("female");
+
+	if(day <= 0 || day > 31){
+		alert("Enter valid day")
+	}
+	else if(month <=0 || month >12 || (month == 2 && day >29)){
+		alert("Enter valid month..")
+	}
 }
