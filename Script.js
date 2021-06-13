d417cc9669d4c31d794f3a6b23af9f77fe2feae1
Muturi403 <faith.muturi@student.moringaschool.com>
2021-06-13 16:32:59 +0300

    this will add bussiness logic in the js script

---
+++ Script.js
@@ -0,0 +1,12 @@
+// Business logic
+var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sartuday"];
+var malesNames = ["Kwasi", "Kwandwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleNames = ["Akosua", "Adoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+
+// user interface logic
+function findNames(e){
+	e.preventDefault();
+	// alert("e");
+	
+}
