ad6ac4fc2bfab01a2752f06204e1046f07053aa3
Muturi403 <faith.muturi@student.moringaschool.com>
2021-06-13 23:31:03 +0300

    this will add the calculation of the Akan names and give a result

--- Script.js
+++ Script.js
@@ -20,4 +20,23 @@ function findNames(e){
 	else if(month <=0 || month >12 || (month == 2 && day >29)){
 		alert("Enter valid month..")
 	}
+
+	// var a = Math.floor((14 - month) / 12)
+	// var y = year -a
+	// var m = month + 12 * a - 2
+	// var d = (day + y + Math.floor(y / 4) - )
+
+	var newDate = new Date(day + "/" + month + "/" + year);
+	var d = newDate.getDay()
+
+	if (male.checked == true){
+		alert("You were born on" + daysOfTheWeek[d] + "and your Akan name is:" + maleNames[d]);
+		document.getElementById('akan').innerHTML = "You were born on " + daysOfTheWeek[d] + "and your Akan name is: " + maleNames[d] ;
+	}
+	else if (male.checked == true){
+		alert("You were born on" + daysOfTheWeek[d] + "and your Akan name is:" + femaleNames[d]);
+		document.getElementById('akan').innerHTML = "You were born on " + daysOfTheWeek[d] + "and your Akan name is: " + femaleNames[d] ;
+
+		//do something
+	}
 }
